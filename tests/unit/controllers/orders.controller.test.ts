d2e73c6f994711be96ba13f52d2dca620632799f
Helio Morais <heliogov20@gmail.com>
2023-09-13 17:38:53 -0300

    Requisito 3

--- tests/unit/controllers/orders.controller.test.ts
+++ tests/unit/controllers/orders.controller.test.ts
@@ -2,6 +2,10 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
+import { ServiceResponse } from '../../../src/types/ServiceType';
+import ordersMocks from '../../mocks/orders.mocks';
+import ordersService from '../../../src/services/orders.service';
+import ordersController from '../../../src/controllers/orders.controller';
 
 chai.use(sinonChai);
 
@@ -13,6 +17,20 @@ describe('OrdersController', function () {
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns(res);
     sinon.restore();
+  });
+
+  it('Testa se retorna successful, status 200, ao listar orders', async function () {
+    const serviceResponse: ServiceResponse<Array<object>> = {
+      status: 'SUCCESSFUL',
+      data: ordersMocks.getAllResponse,
+    };
+
+    sinon.stub(ordersService, 'getAll').resolves(serviceResponse);
+
+    await ordersController.getAll(req,res);
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(ordersMocks.getAllResponse);
   });
 
 });
