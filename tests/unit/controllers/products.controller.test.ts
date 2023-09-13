cad163816a2be7b2a38f328fa4e48dc7d85d0bf1
Helio Morais <heliogov20@gmail.com>
2023-09-13 15:50:47 -0300

    Requisito 1

--- tests/unit/controllers/products.controller.test.ts
+++ tests/unit/controllers/products.controller.test.ts
@@ -2,6 +2,11 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
+import productsMocks from '../../mocks/products.mocks';
+import { ServiceResponse } from '../../../src/types/ServiceType';
+import { Product } from '../../../src/types/Product';
+import productsService from '../../../src/services/products.service';
+import productsController from '../../../src/controllers/products.controller';
 
 chai.use(sinonChai);
 
@@ -13,6 +18,22 @@ describe('ProductsController', function () {
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns(res);
     sinon.restore();
+  });
+
+  it('Testa se retorna created, status 201, ao cadastrar um novo produto', async function () {
+    req.body = productsMocks.createMock;
+
+    const serviceResponse: ServiceResponse<Product> = {
+      status: 'CREATED',
+      data: productsMocks.createResponse,
+    }
+
+    sinon.stub(productsService, 'create').resolves(serviceResponse);
+
+    await productsController.create(req,res);
+
+    expect(res.status).to.have.been.calledWith(201);
+    expect(res.json).to.have.been.calledWith(productsMocks.createResponse);
   });
 
 });
